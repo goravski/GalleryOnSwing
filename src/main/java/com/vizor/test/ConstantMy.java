0c5cd31a5747e715d1062444ef8edd36fd5d0e35
Evgen Goravski <evgoravsky@gmail.com>
2022-04-02 19:43:52 +0300

    Options realized: Add button for download gallery.

--- src/main/java/com/vizor/test/ConstantMy.java
+++ src/main/java/com/vizor/test/ConstantMy.java
@@ -1,19 +1,12 @@
 package com.vizor.test;
 
 public enum ConstantMy {
-    /**
-     * Frame size
-     */
+
     WIDTH(1024),
     HEIGHT(768),
-    /**
-     * Icon size
-     */
+
     SCALE_WIDTH(120),
-    SCALE_HEIGHT(120),
-    /**
-     * Pagination params
-     */
+
     ROWS(3),
     COLUMNS(4),
     GAPS(10);
--- src/main/java/com/vizor/test/Main.java
+++ src/main/java/com/vizor/test/Main.java
@@ -10,9 +10,18 @@ public class Main extends JFrame {
         JFrame frame = new JFrame("DT Developer Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(ConstantMy.WIDTH.getConstant(), ConstantMy.HEIGHT.getConstant()));
-        GridLayoutConstructButtonPanel construct = new GridLayoutConstructButtonPanel();
-        construct.buttonsPanelConstruct(frame);
-        construct.galleryButtonConstruct(frame);
+
+        JPanel startPanel = new JPanel();
+        JButton startButton = new JButton("View gallery");
+        startButton.addActionListener(e -> {
+            frame.getContentPane().remove(0);
+            GridLayoutConstructButtonPanel construct = new GridLayoutConstructButtonPanel();
+            construct.buttonsPanelConstruct(frame);
+            construct.galleryButtonConstruct(frame);
+            frame.setVisible(true);
+        });
+        startPanel.add(startButton);
+        frame.getContentPane().add(startPanel);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
